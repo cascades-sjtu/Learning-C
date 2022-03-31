fe348940641667107a3c9bdaa6da73f533485ba7
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-31 22:13:46 +0800

    add malloc and change scanf's argument

--- Assignment-03/assignment3-extra.c
+++ Assignment-03/assignment3-extra.c
@@ -6,12 +6,16 @@ int main(void) {
   char *first;
   char *last;
 
+  // allocate memory for vars
+  first = malloc(6);
+  last = malloc(6);
+
   // prompt user to input first and last name and use scanf() to store those to
   // the initiliazed vars
   printf("Enter your first name: ");
-  scanf("%ms", &first);
+  scanf("%6s", first);
   printf("Enter your last name: ");
-  scanf("%ms", &last);
+  scanf("%6s", last);
 
   // print the welcome message!
   printf("Hello %s %s!\n", first, last);
